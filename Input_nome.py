baf1c46f1f716b5ab7bcfd02c77b27b09739a3a4
Bela357 <isabelamendesg20@gmail.com>
2025-06-04 21:25:08 -0300

    Funcao excluir cliente por CPF

--- Input_nome.py
+++ Input_nome.py
@@ -5,7 +5,7 @@ def menu():
     print("3 - Aluguéis")
     print("4 - Relatórios")
     print("5 - Sair")
-    op = input("Insira uma opçaõ entre as fornecidas anteriormente:")
+    op = int(input("Insira uma opçaõ entre as fornecidas anteriormente:"))
     return op
 
 def submenu_cliente():
@@ -15,8 +15,9 @@ def submenu_cliente():
     print("3 - Incluir")
     print("4 - Alterar")
     print("5 - Excluir")
-    op = input("Selecione uma opção entre as fornecidas:")
-    opcoes_cliente(op)
+    print("6 - Sair")
+    op = int(input("Selecione uma opção entre as fornecidas:"))
+    return op
 
 def submenu_veiculo():
     print("Submenu veículo")
@@ -25,7 +26,8 @@ def submenu_veiculo():
     print("3 - Incluir")
     print("4 - Alterar")
     print("5 - Excluir")
-    op = input("Selecione uma opção entre as fornecidas:")
+    print("6 - Sair")
+    op = int(input("Selecione uma opção entre as fornecidas:"))
 
 def submenu_aluguel():
     print("Submenu aluguel")
@@ -34,17 +36,26 @@ def submenu_aluguel():
     print("3 - Incluir")
     print("4 - Alterar")
     print("5 - Excluir")
-    op = input("Selecione uma opção entre as fornecidas:")
+    print("6 - Sair")
+    op = int(input("Selecione uma opção entre as fornecidas:"))
 
 def submenu_Relatórios():
     print("1 - Todas as reservas de um cliente")
     print("2 - Todas as reservas do veículo")
     print("3 - Reservas por data")
+    print("6 - Sair")
+    op = int(input("Selecione uma opção entre as fornecidas:"))
 
-def opcoes_cliente(Int):
-    dic_clientes = {}
-    dados = []
-    if Int == 3:
+def opcoes_cliente(valor, dic_clientes, dic_dados):
+    if valor == 1:
+        print(dic_clientes)
+    if valor == 2:
+        cpf = input("Insira o CPF do cliente: ")
+        if cpf in dic_clientes:
+            print(dic_clientes[cpf.values()])
+        else:
+            print("Usuário não encontrado.")
+    elif valor == 3:
         print("Incluir cliente no sistema")
         cpf = input("CPF: ")
         nome = input("Nome completo: ")
@@ -52,18 +63,13 @@ def opcoes_cliente(Int):
         telefone_fix = input("Telefone fixo: ")
         cel = input("Telefone celular: ")
         data_nasc = input("Data de nascimento: ")
-        dados.append(nome, endereco, telefone_fix, cel, data_nasc)
-        dic_clientes[cpf] = dados
-
-    if Int == 1:
-        print(dic_clientes)
-    if Int == 2:
-        cpf = input("Insira o CPF do cliente: ")
-        if cpf in dic_clientes:
-            print(dic_clientes[cpf.values()])
-        else:
-            print("Usuário não encontrado.")
-    if Int == 4:
+        dic_dados["Nome"] = nome
+        dic_dados["Endereço"] = endereco
+        dic_dados["Telefone Fixo"] = telefone_fix
+        dic_dados["Celular"] = cel
+        dic_dados["Data de nascimento"] = data_nasc
+        dic_clientes[cpf] = dic_dados
+    elif valor == 4:
         op = alterar_cilente()
         if op == 1:
             cpf = input("Selecione o CPF a ser alterado: ")
@@ -74,10 +80,21 @@ def opcoes_cliente(Int):
                 else:
                     dic_clientes[novo_cpf] = dic_clientes[cpf]
                     del dic_clientes[cpf]
-        if op == 2:
+        elif op == 2:
             nome = input("Nome a ser alterado: ")
             if nome in dic_clientes:
                 novo_nome = input("Novo nome: ")
+
+    elif valor == 5:
+        cpf = input("Digite o CPF do cliente a ser excluído: ")
+        if cpf in dic_clientes:
+            del dic_clientes[cpf]
+            print("Cliente excluído com sucesso!")
+            print(dic_clientes)
+        else:
+            print("CPF não cadastrado")
+    elif valor == 6:
+        menu()
 
 
     def alterar_cilente():
@@ -91,11 +108,14 @@ def opcoes_cliente(Int):
         op = input("Selecione uma das opções a cima: ")
 
 def main():
+    dic_clientes = {}
+    dic_dados = {}
     option = menu()
     option = 1
     while option != 5:
         if option == 1:
-            submenu_cliente()
+            valor = submenu_cliente()
+            opcoes_cliente(valor,dic_clientes,dic_dados)
         elif option == 2:
             submenu_veiculo()
         elif option == 3:
